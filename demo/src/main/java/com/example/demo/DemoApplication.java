7262cf8b4372e297bcc2d09603770192edb5fe89
juanicastells <juan-c65@live.com.ar>
2023-10-01 17:09:19 -0300

    correccion de errores

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*Esta anotación se utiliza para marcar una clase principal de la aplicación Spring Boot 
+y suele ubicarse en la clase que contiene el método main() de la aplicación.*/
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hola !");
 	}
 
 }
